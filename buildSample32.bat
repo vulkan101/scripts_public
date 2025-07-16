02a8153d9db0d01a34a2e0c255a43fc49758f7d5
Mike Hudson <mikehudsonuk@icloud.com>
2025-07-16 13:19:34 +0100

    Correct WasmEnableMemory64 for 32bit script

--- buildSample32.bat
+++ buildSample32.bat
@@ -1,4 +1,4 @@
 title Build Sample In-Tree 32
 call set_dotnet_env32.cmd
 cd C:\xr_dev\dotnetFork32\runtime
-.\dotnet.cmd build -bl /p:WasmEnableThreads=false /p:WasmEnableMemory64=true /p:TargetOS=browser /p:TargetArchitecture=wasm /p:Configuration=Debug /t:RunSample .\src\mono\sample\wasm\browser
+.\dotnet.cmd build -bl /p:WasmEnableThreads=false /p:WasmEnableMemory64=false /p:TargetOS=browser /p:TargetArchitecture=wasm /p:Configuration=Debug /t:RunSample .\src\mono\sample\wasm\browser
