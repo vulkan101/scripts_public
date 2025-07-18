3162b6d5478fb315e483ee848c90a4b8049d4fa3
Mike Hudson <mikehudsonuk@icloud.com>
2025-07-18 15:22:33 +0100

    Clean sample scripts

---
+++ cleanSample32.bat
@@ -0,0 +1,4 @@
+title "Clean Sample 32"
+taskkill /f /im dotnet.exe
+rmdir /s /q C:\xr_dev\dotnetFork32\runtime\src\mono\sample\wasm\browser\bin
+rmdir /s /q C:\xr_dev\dotnetFork32\runtime\artifacts\obj\mono\Wasm.Browser.Sample
---
+++ cleanSample64.bat
@@ -0,0 +1,4 @@
+title "Clean Sample 64"
+taskkill /f /im dotnet.exe
+rmdir /s /q C:\xr_dev\dotnetFork\runtime\src\mono\sample\wasm\browser\bin
+rmdir /s /q C:\xr_dev\dotnetFork\runtime\artifacts\obj\mono\Wasm.Browser.Sample
